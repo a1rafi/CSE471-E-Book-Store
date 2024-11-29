7c726b7574e766e5f6cfd73e575a375fb26ce76d
Kohinoor Sultana Elora <158803065+eloranoor@users.noreply.github.com>
2024-11-29 12:02:19 +0600

    Favorite Books, Cart & Create Order Functionality API

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const dotenv = require("dotenv").config();
 const userRoute = require('./routes/userroute');
 const bookRoute = require('./routes/bookroute');
+const favouriteRoute = require('./routes/favouriteroute');
+const cartRoute = require('./routes/cartroute');
+const orderRoute = require('./routes/orderroute');
+
+
 require('./conn/conn');
 
 const app = express();
@@ -19,3 +24,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/user', userRoute, bookRoute);
+app.use('/api/user', userRoute, favouriteRoute);
+app.use('/api/user', userRoute, cartRoute);
+app.use('/api/user', userRoute, orderRoute);
+
+
+
